6665535761aeefb6b8afffaa62d320f125565dd2
Bobby Galli <bobbyg603@gmail.com>
2020-04-12 16:21:50 -0400

    Removed mock-multi-select-pipe.

--- projects/filterable-dropdown-example/src/app/app.component.spec.ts
+++ projects/filterable-dropdown-example/src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
-import { TestBed, async } from '@angular/core/testing';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { async, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        MockFilterableDropdownComponent
       ],
     }).compileComponents();
   }));
@@ -26,6 +28,21 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('filterable-dropdown-example app is running!');
+    expect(compiled.querySelector('h2').textContent).toContain('filterable-dropdown-example');
   });
 });
+
+@Component({
+  selector: 'bugsplat-filterable-dropdown',
+  template: ''
+})
+class MockFilterableDropdownComponent {
+  @Input() allowMultiSelect: any;
+  @Input() autoClose: any;
+  @Input() items: any;
+  @Input() selectedItems: any;
+  @Input() disabled: any;
+  @Input() placeholder: any;
+  @Output() onItemsSelected = new EventEmitter<any>();
+  @Output() onOpen = new EventEmitter<any>();
+}
