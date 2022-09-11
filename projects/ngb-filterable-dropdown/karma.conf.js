2612243238ec4b07995b8daa5290ed6b41f03ac8
Bobby Galli <bobbyg603@gmail.com>
2022-09-11 16:42:53 -0400

    chore: fix karma error for ci

--- projects/ngb-filterable-dropdown/karma.conf.js
+++ projects/ngb-filterable-dropdown/karma.conf.js
@@ -25,7 +25,16 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: [
+      'Chrome',
+      'ChromeHeadlessCI'
+    ],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true,
     proxies: {
